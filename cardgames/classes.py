3be9dee2730733c9a20dfe3f1e30b880cf4ca2ba
James Webb <james@ultra-horizon.com>
2020-04-13 14:54:29 +0100

    Minor improvments

--- cardgames/classes.py
+++ cardgames/classes.py
@@ -15,7 +15,8 @@ class Card():
         else:
             return self.value + " of " + self.suit + " "
 
-#my cards represent the cards that each player has as a list. The dealer can also access my cards, but deals from deck.
+
+# my cards represent the cards that each player has as a list. The dealer can also access my cards, but deals from deck.
 class Player():
 
     def __init__(self, name, inital_bet=0):
@@ -28,78 +29,88 @@ class Player():
         # players who have said no to a split earlier in game cannot split later. Value turns false if player did not want to split earlier in game.
         self.number_of_hands = 1
         self.said_split = True
-        self.over_twentyone=False
+        self.over_twentyone = False
 
-
-    def get_value(self, player_deck: list[Card]):
+    def get_value(self, player_deck: [Card]):
         total_value = 0
-        for _ in player_deck:
-            total_value+=Card().value
+        for card in player_deck:
+            total_value += card.value
             if total_value > 21:
                 print("card values are over 21, you have lost")
-                self.freeze()
+                # self.freeze()
                 self.over_twentyone = True
             else:
-                print("card values are under 21, you're still in the game")e
+                print("card values are under 21, you're still in the game")
         return total_value
 
-    def check_blackjack(self, player_deck:list[Card]):
-        if player_deck.count==2 and (for card in player_deck if card.name == "Ace" and ("Queen" or "King" or "Jack")):
-        print("You've won blackjack")
-        player_wins_cash = self.bet_per_hand*1.5
-        return player_wins_cash
+    def check_blackjack(self, player_deck: [Card]):
+        ace, royal = False, False
+        if len(player_deck) == 2:
+            for card in player_deck:
+                if card.name == 'Ace':
+                    ace = True
+                if card.name in ["Queen", "King", "Jack"]:
+                    royal = True
+        if ace and royal:
+            print("You've won blackjack")
+            player_wins_cash = self.bet_per_hand * 1.5
+            # self.freeze()
+            return player_wins_cash
 
-    def inital_deal(self, dealer_deck: list[Card], player_deck: list[Card]):
+    def inital_deal(self, dealer_deck: [Card], player_deck: [Card]):
         player_deck.append(dealer_deck.pop)
         player_deck.append(dealer_deck.pop)
-        print(player_deck[Card.__str__()])
+        print([card.__str__() for card in player_deck])
         return player_deck
 
-
-    def hit(self, dealer_deck: list[Card], player_deck: list[Card]):
+    def hit(self, dealer_deck: [Card], player_deck: [Card]):
         player_deck.append(dealer_deck.pop)
-        print(player_deck[Card.__str__()])
+        print([card.__str__() for card in player_deck])
         return player_deck
 
-    def stay(self, player_deck: list[Card], ace_value=1):
+    def stay(self, player_deck: [Card], ace_value=1):
+        if ace_value not in [1, 11]:
+            print("Invalid ace value")
         for card in player_deck:
-            if card.name=="Ace":
-                card.value=ace_value
-            else: print("No Aces in the set")
-
+            if card.name == "Ace":
+                card.value = ace_value
+            else:
+                print("No Aces in the set")
 
     def not_split(self):
         print("This player will not be able to split in the future")
         return False
 
-def split(self, player_deck: list[Card]):
-    if self.said_split==True
-        if player_deck[-1]==player_deck[-2]
-            self.number_of_hands+=1
-            if self.number_of_hand == 2:
-                split_my_cards = []
-                split_my_cards = split_my_cards.append(player_deck.pop)
-                return split_my_cards
-            if self.number_of_hands == 3:
-                split2_my_cards = []
-                split2_my_cards = split2_my_cards.append(split_my_cards.pop)
-                return split2_my_cards
-        else: print("no identical last cards for splitting")
-    else: print("cannot split cards because said no to splitting earlier in game")
+    def split(self, player_deck: [Card]):
+        if self.said_split == True:
+            if player_deck[-1] == player_deck[-2]:
+                self.number_of_hands += 1
+                split_my_cards, split2_my_cards = [], []
+                if self.number_of_hands == 2:
+                    split_my_cards = split_my_cards.append(player_deck.pop())
+                    return split_my_cards
+                if self.number_of_hands == 3:
+                    split2_my_cards = split2_my_cards.append(split_my_cards.pop())
+                    return split2_my_cards
+            else:
+                print("no identical last cards for splitting")
+        else:
+            print("cannot split cards because said no to splitting earlier in game")
 
+        # after inital deal or after the first two cards of any split pair, if the cards have a hard total of 9, 10 or 11,
+        # the player can choose to only have one more card played to them and to double their wager
 
-    #after inital deal or after the first two cards of any split pair, if the cards have a hard total of 9, 10 or 11,
-    # the player can choose to only have one more card played to them and to double their wager
-def double_down(self, dealer_deck: list[Card], player_deck: list[Card]):
-    self.money_out=self.bet_per_hand*2
-    player_deck.append(dealer_deck.pop)
-    return player_deck
+    def double_down(self, dealer_deck: [Card], player_deck: [Card]):
+        self.money_out = self.bet_per_hand * 2
+        player_deck.append(dealer_deck.pop)
+        return player_deck
 
-def money_money_money(self,dealer_deck: list[Card], player_deck: list[Card]):
-    if get_value(dealer_deck)>get_value(player_deck)
-        winning_money = Dealer.bet_per_hand * 2
-        loosing_money =
-        print "Dealer wins " +str((Dealer.bet_per_hand*2))
+    def money_money_money(self, dealer, player_deck: [Card]):
+        if self.get_value(dealer.deck) > self.get_value(player_deck):
+            winning_money = dealer.bet_per_hand * 2
+            print("Dealer wins " + str((dealer.bet_per_hand * 2)))
+            return winning_money
+
 
 class Dealer(Player):
 
@@ -108,11 +119,11 @@ class Dealer(Player):
         self.number_of_decks = number_of_decks
         self.deck = self.create_deck(self.number_of_decks)
         self.deck = shuffle(self.deck)
-        self.last_card=self.pop_card(self.deck)
+        self.last_card = self.pop_card(self.deck)
 
     def create_deck(self, deck_multiplier):
         card_deck = []
-        for x in range (1, deck_multiplier+1, 1):
+        for x in range(1, deck_multiplier + 1, 1):
             for suit in ["Hearts", "Diamonds", "Spades", "Clubs"]:
                 card_deck.append(Card(suit, 1, name="Ace"))
                 for royal in ["Jack", "Queen", "King"]:
@@ -121,24 +132,23 @@ class Dealer(Player):
                     card_deck.append(Card(suit, i))
         return card_deck
 
+    def shuffle(self, cards_to_shuffle):
+        shuffle(cards_to_shuffle)
+        return cards_to_shuffle
 
-def shuffle(cards_to_shuffle):
-    shuffle(cards_to_shuffle)
-    return cards_to_shuffle
 
-
-Marina=Dealer(marina,)
+Marina = Dealer(marina, )
 print()
 
-    # # I am a getter for the @properties
-    # @property
-    # def name(self):
-    #     return self._name
-    #
-    # @name.setter
-    # def name(self, newname):
-    #     self._name = newname
-    #
-    # @name.deleter
-    # def name(self):
-    #     del self._name
+# # I am a getter for the @properties
+# @property
+# def name(self):
+#     return self._name
+#
+# @name.setter
+# def name(self, newname):
+#     self._name = newname
+#
+# @name.deleter
+# def name(self):
+#     del self._name
