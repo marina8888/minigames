e0133ae97e19f0957b73b68204349858f193fcbf
Marina Kovaleva <38610356+marina8888@users.noreply.github.com>
2020-04-13 19:08:03 +0900

    player class complete no betting values yet

--- cardgames/classes.py
+++ cardgames/classes.py
@@ -28,32 +28,67 @@ class Player():
         # players who have said no to a split earlier in game cannot split later. Value turns false if player did not want to split earlier in game.
         self.number_of_hands = 1
         self.said_split = True
+        self.over_twentyone=False
 
-    def pop_card(self, Dealer_deck, my_cards):
-        last_card = Dealer_deck.pop()
-        return my_cards.append(last_card)
 
-    def inital_deal(self,  Dealer_deck, my_cards):
-        my_cards= self.pop_card(Dealer_deck, my_cards)
-        my_cards = self.pop_card(Dealer_deck, my_cards)
-        return my_cards
+    def get_value(self, player_deck: list[Card]):
+        total_value = 0
+        for _ in player_deck:
+            total_value+=Card().value
+            if total_value > 21:
+                print("card values are over 21, you have lost")
+                self.freeze()
+                self.over_twentyone = True
+            else:
+                print("card values are under 21, you're still in the game")e
+        return total_value
 
-    def hit(self, Dealer_deck, my_cards):
-        return self.pop_card(Dealer_deck, my_cards)
 
-    def stay(self):
-            pass
+    def inital_deal(self, dealer_deck, player_deck: list[Card]):
+        player_deck.append(dealer_deck.pop)
+        player_deck.append(dealer_deck.pop)
+        print(player_deck[Card.__str__()])
+        return player_deck
 
-    def split(self):
-        if my_cards
 
-    def win_or_loose(self):
-        pass
+    def hit(self, dealer_deck: list[Card], player_deck: list[Card]):
+        player_deck.append(dealer_deck.pop)
+        print(player_deck[Card.__str__()])
+        return player_deck
+
+    def stay(self, player_deck: list[Card], ace_value=1):
+        for card in player_deck:
+            if card.name=="Ace":
+                card.value=ace_value
+            else: print("No Aces in the set")
+
+
+    def not_split(self):
+        print("This player will not be able to split in the future")
+        return False
+
+def split(self, player_deck: list[Card]):
+    if self.said_split==True
+        if player_deck[-1]==player_deck[-2]
+            self.number_of_hands+=1
+            if self.number_of_hand == 2:
+                split_my_cards = []
+                split_my_cards = split_my_cards.append(player_deck.pop)
+                return split_my_cards
+            if self.number_of_hands == 3:
+                split2_my_cards = []
+                split2_my_cards = split2_my_cards.append(split_my_cards.pop)
+                return split2_my_cards
+        else: print("no identical last cards for splitting")
+    else: print("cannot split cards because said no to splitting earlier in game")
+
 
     #after inital deal or after the first two cards of any split pair, if the cards have a hard total of 9, 10 or 11,
     # the player can choose to only have one more card played to them and to double their wager
-    def double_down(self):
-        pass
+def double_down(self, dealer_deck: list[Card], player_deck: list[Card]):
+    self.bet=self.bet*2
+    player_deck.append(dealer_deck.pop)
+    return player_deck
 
 class Dealer(Player):
 
@@ -64,10 +99,11 @@ class Dealer(Player):
         self.deck = self.shuffle(self.deck)
         self.last_card=self.pop_card(self.deck)
 
-    def create_deck(self, number_of_decks):
+    def create_deck(self, deck_multiplier):
         card_deck = []
-        for x in range (1, number_of_decks+1, 1):
+        for x in range (1, deck_multiplier+1, 1):
             for suit in ["Hearts", "Diamonds", "Spades", "Clubs"]:
+                card_deck.append(Card(suit, 1, name="Ace"))
                 for royal in ["Jack", "Queen", "King"]:
                     card_deck.append(Card(suit, 10, name=royal))
                 for i in range(1, 11, 1):
@@ -78,6 +114,9 @@ class Dealer(Player):
     def shuffle(cards_to_shuffle):
         shuffle(cards_to_shuffle)
         return cards_to_shuffle
+
+    def win_or_loose(self):
+        pass
 
     def pay_to_player(self):
         pass
