e7f839708cf5cf4d5c57e8115b4ee39e905ce861
Marina Kovaleva <38610356+marina8888@users.noreply.github.com>
2020-04-13 15:41:46 +0900

    added class skeleton for blackjack.py to use

--- cardgames/classes.py
+++ cardgames/classes.py
@@ -15,56 +15,77 @@ class Card():
         else:
             return self.value + " of " + self.suit + " "
 
-
+#my cards represent the cards that each player has as a list. The dealer can also access my cards, but deals from deck.
 class Player():
 
-    def __init__(self, name):
+    def __init__(self, name, inital_bet=0):
         self.name = name
+        self.bet=inital_bet
         self.my_cards = []
 
-    def bet(self):
+        # split function allows player to split their card deck.
+        # number_of_hands value signifies how many sets a player has (up to a maximum of three as players cannot split more than twice per game)
+        # players who have said no to a split earlier in game cannot split later. Value turns false if player did not want to split earlier in game.
+        self.number_of_hands = 1
+        self.said_split = True
 
+    def pop_card(self, Dealer_deck, my_cards):
+        last_card = Dealer_deck.pop()
+        return my_cards.append(last_card)
 
-    def hit(self, card):
-        Player.table_deck.append(card)
+    def inital_deal(self,  Dealer_deck, my_cards):
+        my_cards= self.pop_card(Dealer_deck, my_cards)
+        my_cards = self.pop_card(Dealer_deck, my_cards)
+        return my_cards
+
+    def hit(self, Dealer_deck, my_cards):
+        return self.pop_card(Dealer_deck, my_cards)
 
     def stay(self):
-        pass
+            pass
 
     def split(self):
+        if my_cards
 
+    def win_or_loose(self):
+        pass
+
+    #after inital deal or after the first two cards of any split pair, if the cards have a hard total of 9, 10 or 11,
+    # the player can choose to only have one more card played to them and to double their wager
+    def double_down(self):
+        pass
 
 class Dealer(Player):
-    def __init__(self, name):
-        super().__init__(name)
-        self.deck = self.create_deck()
-        self.deck = self.shuffle()
 
-    @staticmethod
-    def create_deck():
+    def __init__(self, name, number_of_decks=1):
+        super().__init__(name)
+        self.number_of_decks = number_of_decks
+        self.deck = self.create_deck(self.number_of_decks)
+        self.deck = self.shuffle(self.deck)
+        self.last_card=self.pop_card(self.deck)
+
+    def create_deck(self, number_of_decks):
         card_deck = []
-        for suit in ["Hearts", "Diamonds", "Spades", "Clubs"]:
-            for royal in ["Jack", "Queen", "King"]:
-                card_deck.append(Card(suit, 10, name=royal))
-            for i in range(1, 11, 1):
-                card_deck.append(Card(suit, i))
+        for x in range (1, number_of_decks+1, 1):
+            for suit in ["Hearts", "Diamonds", "Spades", "Clubs"]:
+                for royal in ["Jack", "Queen", "King"]:
+                    card_deck.append(Card(suit, 10, name=royal))
+                for i in range(1, 11, 1):
+                    card_deck.append(Card(suit, i))
         return card_deck
 
-    def shuffle(self):
-        shuffle(self.deck)
-        return self.deck
-
-    def deal_to_player(self,):
-        pass
+    @staticmethod
+    def shuffle(cards_to_shuffle):
+        shuffle(cards_to_shuffle)
+        return cards_to_shuffle
 
     def pay_to_player(self):
         pass
 
-    def
     def pay_to_self(self):
 
-
-
+Marina=Dealer(marina,)
+print()
 
     # # I am a getter for the @properties
     # @property
