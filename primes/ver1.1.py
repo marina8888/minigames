eeeab6b07f777614b5f9dc72cffac78082ea018f
Marina Kovaleva <38610356+marina8888@users.noreply.github.com>
2020-04-18 13:30:09 +0900

    check second digit added

--- primes/ver1.1.py
+++ primes/ver1.1.py
@@ -5,14 +5,17 @@
 max_val = input("please select up to which number you want to check primes: ")
 primes_list = [2]
 appendable: bool = True
-
+non_primelist=[2,4,5,6,8,0]
 for i in range (3,int(max_val)+1,1):
-    for prime in primes_list:
-        if i%prime==0:
-            appendable=False
+    if i % 10 in non_primelist:
+        i+=1
 
-    if appendable==True:
-        primes_list.append(i)
-    appendable=True
+        for prime in primes_list:
+            if i%prime==0:
+                appendable=False
+
+        if appendable==True:
+            primes_list.append(i)
+        appendable=True
 
 print(primes_list)
