6f42ad3f072bc663dd33d88f92efd45d918f9e09
Marina Kovaleva <marinakovaleva0@gmail.com>
2020-04-19 23:58:04 +0900

    time counter added

--- primes/ver2.1.py
+++ primes/ver2.1.py
@@ -1,27 +1,26 @@
 
 import math
 import time
-
+import numpy as np
 #max_val as the maximum value for the prime
 #min_val as the minimum value for the prime
 
 max_val = input("please select up to which number you want to check primes: ")
 t1=time.monotonic()
-primes_list= [3,7]
-iter=0
+primes_list= np.array([2])
 appendable: bool = True
 prime_end={1,3,7,9}
 
-for i in range (11,int(max_val),1):
+@njit
+def marina(list, )
+    for i in range (2,int(max_val)+1,1):
     if i % 10 in prime_end:
-        iter = 0
-        while primes_list[iter]<math.sqrt(i):
-            if i%primes_list[iter]==0:
+        for primes in primes_list[primes_list<math.sqrt(i)]:
+            if i%primes==0:
                 appendable=False
-            iter += 1
         if appendable==True:
-            primes_list.append(i)
-        appendable=True
+            primes_list=np.append(primes_list, i)
+    appendable=True
 t2=time.monotonic()
-print(primes_list[-1])
+print(primes_list)
 print(t2-t1)
