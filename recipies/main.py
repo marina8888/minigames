99f38ebdcdff8b4af77a25c4566061698735bb88
James Webb <james@ultra-horizon.com>
2020-05-03 15:10:57 +0100

    Convert recipe data to dataframe object

--- recipies/main.py
+++ recipies/main.py
@@ -1,4 +1,9 @@
-import os, json
+import json
+import os
+
+import numpy as np
+import pandas as pd
+
 
 def get_recipes():
     json_files = [file for file in os.listdir('./recipes') if file.endswith('.json')]
@@ -9,10 +14,29 @@ def get_recipes():
     return recipes
 
 
+def create_dataframe(recipes):
+    recipe_names = [recipe['name'] for recipe in recipes]
+    ingredients = []
+    for recipe in recipes:
+        ingredients += recipe['ingredients'].keys()
+    ingredients = list(set(ingredients))
+    df = pd.DataFrame(columns=recipe_names, index=ingredients)
+    for ingredient in ingredients:
+        for recipe in recipes:
+            ingred_info = recipe['ingredients'].get(ingredient)
+            if ingred_info is not None:
+                data = str(ingred_info['quantity']) + ' ' + ingred_info['unit']
+            else:
+                data = ""
+            df.loc[ingredient][recipe['name']] = data
+    return df
+
+
 def main():
     recipes = get_recipes()
-    for recipe in recipes:
-        print(recipe['name'])
+    df = create_dataframe(recipes)
+    print(df)
+
 
 if __name__ == "__main__":
     main()
